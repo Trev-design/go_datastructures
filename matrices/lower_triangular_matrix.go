fd49b316539b2a2b533bed9496956bc249e3fb34
Tev-design <gerrit.flick@gmail.com>
2024-02-20 21:45:08 +0100

    lower triangular matrix

--- matrices/lower_triangular_matrix.go
+++ matrices/lower_triangular_matrix.go
@@ -1,1 +1,63 @@
 package matrices
+
+import (
+	"fmt"
+	"math"
+)
+
+type LowTriMatrix struct {
+	container []int
+	dimension int
+}
+
+func CreateLowTriMatrix(dimension int) *LowTriMatrix {
+	return &LowTriMatrix{
+		container: make([]int, dimension*(dimension+1)/2),
+		dimension: dimension,
+	}
+}
+
+func (matrix *LowTriMatrix) SetValue(row, column, item int) error {
+	if row > matrix.dimension {
+		return fmt.Errorf("row index to big")
+	}
+
+	if column > matrix.dimension {
+		return fmt.Errorf("column index to big")
+	}
+
+	if row >= column {
+		matrix.container[row*(row-1)/2+column-1] = item
+	}
+
+	return nil
+}
+
+func (matrix *LowTriMatrix) GetValue(row, column int) (int, error) {
+	if row >= matrix.dimension {
+		return math.MinInt, fmt.Errorf("row index to big")
+	}
+
+	if column >= matrix.dimension {
+		return math.MinInt, fmt.Errorf("column index to big")
+	}
+
+	if row >= column {
+		return matrix.container[row*(row-1)/2+column-1], nil
+	}
+
+	return math.MinInt, nil
+}
+
+func (matrix *LowTriMatrix) ShowMatrix() {
+	for row := 1; row <= matrix.dimension; row++ {
+		for column := 1; column <= matrix.dimension; column++ {
+			if row >= column {
+				fmt.Printf(" %d", matrix.container[row*(row-1)/2+column-1])
+			} else {
+				fmt.Print(" 0")
+			}
+		}
+		fmt.Println()
+	}
+}
--- matrices/matrix_playground.go
+++ matrices/matrix_playground.go
@@ -31,4 +31,16 @@ func MatrixUtils() {
 	if _, err := dima.GetValue(2, 8); err != nil {
 		fmt.Printf("%v\n", err)
 	}
+
+	lowma := CreateLowTriMatrix(7)
+
+	for row := 1; row <= 7; row++ {
+		for column := 1; column <= 7; column++ {
+			if err := lowma.SetValue(row, column, row+column); err != nil {
+				fmt.Printf("%v\n", err)
+			}
+		}
+	}
+
+	lowma.ShowMatrix()
 }
